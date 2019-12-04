cb3f0037a156ba919add65803d740b58bba03254
unknown <phanthanhvuong3@gmail.com>
2019-12-04 16:01:45 +0700

    res

--- FrontEnd/dangki.js
+++ FrontEnd/dangki.js
@@ -1,25 +1,40 @@
 const register=()=>{
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const headers = new Headers({
-        'Content-Type': 'application/json',
-      });
-    fetch('http://127.0.0.1:9000/api/users', {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify({
-      role: 'user',
-      access_token: 'masterKey',
-      email:email,
-      password:password
+    const password2=document.getElementById('password2').value;
+
+
+    if(email=='' || password==''){
+      window.location.reload('dangky.html');
+    }else if(password2 !=password){
+        window.location.reload('dangky.html');
+    }else
+    {
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+        });
+        fetch('http://127.0.0.1:9000/api/users', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+        role: 'user',
+        access_token: 'masterKey',
+        email:email,
+        password:password
     })
   })
   .then(result => result.json())
   .then(user => {
-    console.log(user);
-   window.location.replace('dangnhap.html');
+    
+    console.log(user.email);
+    if(user.email==undefined){
+        window.location.reload('dangky.html');
+    }else{
+        window.location.replace('dangnhap.html');
+    }
   })
-  .catch(err => {
-    console.log(err);
+     .catch(err => {
+      console.log(err);
   });
+}
 }
